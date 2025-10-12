59d6758445c26d5e4ee6074aa4b76c48d9df785c
HossainRezayi <hossainrezayi26@gmail.com>
2025-10-12 15:54:12 +0430

    Building the pages

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,7 @@
+import Styles from "./AppNav.module.css";
+
+function AppNav() {
+  return <nav className={Styles.nav}>App Nav</nav>;
+}
+
+export default AppNav;
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,25 @@
 import { NavLink } from "react-router-dom";
+import Style from "./PageNav.module.css";
+import Logo from "./Logo";
 
 function PageNav() {
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/pricing">Pricing</NavLink>
-      </li>
-      <li>
-        <NavLink to="/product">Product</NavLink>
-      </li>
-    </ul>
+    <div className={Style.nav}>
+      <Logo />
+      <ul>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={Style.ctaLink}>
+            Log in
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 }
 
